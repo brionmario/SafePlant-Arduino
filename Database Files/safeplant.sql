8ce852a40227cce7799724e10890f0995f0be4ed
Brion Mario <brion.2015283@iit.ac.lk>
2017-03-27 10:40:21 +0530

    added testimonial table

--- Database Files/safeplant.sql
+++ Database Files/safeplant.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 02, 2017 at 08:55 PM
+-- Generation Time: Mar 27, 2017 at 07:09 AM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 5.6.28
 
@@ -94,6 +94,29 @@ INSERT INTO `social_links` (`id`, `url`, `social_name`) VALUES
 (3, '#', 'google-plus'),
 (4, '#', 'youtube');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `testimonials`
+--
+
+CREATE TABLE `testimonials` (
+  `id` int(5) NOT NULL,
+  `comment` varchar(1024) NOT NULL,
+  `img_url` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `patient_type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `testimonials`
+--
+
+INSERT INTO `testimonials` (`id`, `comment`, `img_url`, `name`, `patient_type`) VALUES
+(1, 'Thank you very much! Everyone is so friendly and I am very thankful you are in this service to care for us when we need you. Your hours you are available are wonderful. The Doctors answers are very helpfull. I highly recommend it.', 'images/patient-img.png', 'Prashantha Dias', 'Transplant Patient'),
+(2, 'Thank you so much for providing such wonderful service! The doctors and their respons were very helpfull and provided me with an exceptional experience. A great service. ', 'images/patient-img.png', 'Sunil Gunawardhana', 'Transplant Patient'),
+(3, 'I want to express my sincere appreciation for the promptness and professional care given to my wife. Our experience at this facility was of the quickest and best we have ever encountered. The device , mobile application and web service assist to get best aftercare, I highly recommend SAFE PLANT for aftercare.', 'images/patient-img.png', 'Joshep Fransisco', 'Care Taker');
+
 --
 -- Indexes for dumped tables
 --
@@ -111,6 +134,12 @@ ALTER TABLE `social_links`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `testimonials`
+--
+ALTER TABLE `testimonials`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -124,6 +153,11 @@ ALTER TABLE `blog_column1_homepage`
 --
 ALTER TABLE `social_links`
   MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `testimonials`
+--
+ALTER TABLE `testimonials`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
