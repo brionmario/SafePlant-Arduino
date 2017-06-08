5a937600946bbd6076d815e161a61a2e021c88f7
brionmario <brion.2015283@iit.ac.lk>
2017-06-08 16:35:44 +0530

    testing login api

--- config/android_login_api/include/DB_Functions.php
+++ config/android_login_api/include/DB_Functions.php
@@ -27,9 +27,13 @@ class DB_Functions {
      * returns user details
      */
     public function storeUser($name, $email, $password) {
+        $uuid = uniqid('', true);
+        $hash = $this->hashSSHA($password);
+        $encrypted_password = $hash["encrypted"]; // encrypted password
+        $salt = $hash["salt"]; // salt
 
-        $stmt = $this->conn->prepare("INSERT INTO users(first_name, user_email, password, created_at) VALUES(?, ?, ?, NOW())");
-        $stmt->bind_param("sssss", $name, $email, $password);
+        $stmt = $this->conn->prepare("INSERT INTO users(unique_id, name, email, encrypted_password, salt, created_at) VALUES(?, ?, ?, ?, ?, NOW())");
+        $stmt->bind_param("sssss", $uuid, $name, $email, $encrypted_password, $salt);
         $result = $stmt->execute();
         $stmt->close();
 
@@ -60,8 +64,12 @@ class DB_Functions {
             $user = $stmt->get_result()->fetch_assoc();
             $stmt->close();
 
+            // verifying user password
+            $salt = $user['salt'];
+            $encrypted_password = $user['encrypted_password'];
+            $hash = $this->checkhashSSHA($salt, $password);
             // check for password equality
-            if ($password == $user['password']) {
+            if ($encrypted_password == $hash) {
                 // user authentication details are correct
                 return $user;
             }
@@ -91,6 +99,32 @@ class DB_Functions {
             $stmt->close();
             return false;
         }
+    }
+
+    /**
+     * Encrypting password
+     * @param password
+     * returns salt and encrypted password
+     */
+    public function hashSSHA($password) {
+
+        $salt = sha1(rand());
+        $salt = substr($salt, 0, 10);
+        $encrypted = base64_encode(sha1($password . $salt, true) . $salt);
+        $hash = array("salt" => $salt, "encrypted" => $encrypted);
+        return $hash;
+    }
+
+    /**
+     * Decrypting password
+     * @param salt, password
+     * returns hash string
+     */
+    public function checkhashSSHA($salt, $password) {
+
+        $hash = base64_encode(sha1($password . $salt, true) . $salt);
+
+        return $hash;
     }
 
 }
--- config/android_login_api/register.php
+++ config/android_login_api/register.php
@@ -30,9 +30,11 @@ if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password'])
         if ($user) {
             // user stored successfully
             $response["error"] = FALSE;
+            $response["uid"] = $user["unique_id"];
             $response["user"]["name"] = $user["name"];
             $response["user"]["email"] = $user["email"];
             $response["user"]["created_at"] = $user["created_at"];
+            $response["user"]["updated_at"] = $user["updated_at"];
             echo json_encode($response);
         } else {
             // user failed to store
