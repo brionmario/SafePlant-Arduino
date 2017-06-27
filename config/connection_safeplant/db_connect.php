236c46e8d3fa7f7de1c6748b818c9e64ba5d9326
brionmario <brion.2015283@iit.ac.lk>
2017-06-28 01:45:59 +0530

    started implementing the feedback feature

--- config/connection_safeplant/db_connect.php
+++ config/connection_safeplant/db_connect.php
@@ -1,23 +1,15 @@
 <!--connection file-->
 <?php
-	$connectstr_dbhost = '';
-$connectstr_dbname = '';
-$connectstr_dbusername = '';
-$connectstr_dbpassword = '';
+	$con=mysqli_connect("127.0.0.1:55112","azure","6#vWHD_$","safeplant_db");
+	//mysql_query("SET NAMES 'utf8'"); 
+	//mysql_query("SET CHARACTER SET 'utf8'");
 
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR") !== 0) {
-        continue;
-    }
-    
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-}
-
-define('DB_NAME', $connectstr_dbname);
-define('DB_USER', $connectstr_dbusername);
-define('DB_PASSWORD', $connectstr_dbpassword);
-define('DB_HOST', $connectstr_dbhost);
+	// Check connection
+	if (mysqli_connect_errno())
+  	{
+  	//echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  	}
+	//echo "<script type='text/javascript'>alert('Connection States : Good !');</script>";
+	//echo  "Connection Successful !" ;
+	
 ?>
