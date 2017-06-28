60c09efd005dd6fdaf895cf0b17ffb16eab8af75
Brion Mario <brion.2015283@iit.ac.lk>
2017-06-29 04:21:31 +0530

    Update db_connect.php

--- config/connection_safeplant/db_connect.php
+++ config/connection_safeplant/db_connect.php
@@ -3,10 +3,6 @@
 	$con=mysqli_connect("127.0.0.1:55112","azure","6#vWHD_$","safeplant_db");
 	//mysql_query("SET NAMES 'utf8'"); 
 	//mysql_query("SET CHARACTER SET 'utf8'");
-<<<<<<< HEAD
-=======
-
->>>>>>> dedc41ade4fe5e8e79c4436d1decbb33e6e67960
 	// Check connection
 	if (mysqli_connect_errno())
   	{
@@ -15,8 +11,4 @@
 	//echo "<script type='text/javascript'>alert('Connection States : Good !');</script>";
 	//echo  "Connection Successful !" ;
 	
-<<<<<<< HEAD
 ?>
-=======
-?>
->>>>>>> dedc41ade4fe5e8e79c4436d1decbb33e6e67960
